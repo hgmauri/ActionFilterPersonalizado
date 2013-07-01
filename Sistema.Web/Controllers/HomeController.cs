1de47c0c62c4d479b71c2122d3dfd7c8d699786a
Henrique Mauri <hgmauri@gmail.com>
2013-07-01 18:58:32 -0300

    Inclusão do Fontes

---
+++ Sistema.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Sistema.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema.Web.Controllers
+{
+    [BaseController]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new Exception("Erro");
+            //return View();
+        }
+    }
+}
